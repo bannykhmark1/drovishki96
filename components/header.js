b37b2a0352dd1e29ccaf94bcb3aad5f7de9b6f5c
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2023-05-10 13:43:28 +0500

    first commit

---
+++ components/header.js
@@ -0,0 +1,21 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <nav>
+          <div></div>
+          <div>
+            <ul className="flex justify-between">
+              <li className="mr-8">Дрова</li>
+              <li className="mr-8">О нас</li>
+              <li className="mr-8">Отзывы</li>
+              <li className="mr-8">Доставка и оплата</li>
+              <li className="mr-8">Контакты</li>
+              <li className="">Полезные статьи</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
